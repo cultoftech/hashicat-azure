ec3afacfb27053ac163a9e7356fb4ee295d7ec95
Victor <generalinq@gmail.com>
2021-02-26 19:37:00 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,5 @@
 module "network" {
   source  = "app.terraform.io/victor-trainingxx/network/azurerm"
-  version = "3.0.1"
-  # insert required variables here
+  version = "3.3.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
