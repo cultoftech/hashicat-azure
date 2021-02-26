26571ec3f0c13eabe5390845f2db7b0240c808ab
Victor <generalinq@gmail.com>
2021-02-26 17:34:40 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
-  location           x = azurerm_resource_group.myresourcegroup.location
+  location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
